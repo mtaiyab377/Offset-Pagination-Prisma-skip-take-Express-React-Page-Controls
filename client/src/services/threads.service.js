e2b33c548bf89a5112bfbc84ec60fc1daefe2a51
mtaiyab377 <mtaiyab377@gmail.com>
2026-09-21 09:24:56 +0530

    Modify getThreads to accept pagination parameter

--- client/src/services/threads.service.js
+++ client/src/services/threads.service.js
@@ -1,6 +1,9 @@
+
 import apiClient from "./apiClient";
 
-export async function getThreads() {
-  const res = await apiClient.get("/api/threads");
-  return res.data.threads;
+export async function getThreads(page = 1) {
+  const res = await apiClient.get("/api/threads", {
+    params: { page },
+  });
+  return res.data;
 }
